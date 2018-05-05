f933fe56be95183ce469ffe5056cd08f14302141
HYH1MM <877573522@qq.com>
2018-05-06 01:58:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hyh/myapplication/ButtonActivity.java
@@ -0,0 +1,13 @@
+package com.example.hyh.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class ButtonActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button);
+    }
+}
